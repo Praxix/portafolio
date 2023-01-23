f41568589c81c7845199244b35e923d0275ab55a
Praxix <jean.galvis01@gmail.com>
2023-01-23 14:53:02 -0500

    crear aplicación, pagina y componentes

---
+++ src/app/pages/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HomePage } from './home.page';
+import { ComponentsModule } from 'src/app/shared/components/components.module';
+
+
+@NgModule({
+  declarations: [
+    HomePage
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    ComponentsModule
+  ]
+})
+export class HomeModule { }
